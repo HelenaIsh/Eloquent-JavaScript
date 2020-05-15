5fa7ef7594bf1f32d497559f97793794cf4b98b4
Elena Ishmatova <elena-ishmatova@mail.ru>
2020-05-15 20:55:18 +0500

    add Chapter 3

---
+++ Chapter 3/Bean counting.js
@@ -0,0 +1,18 @@
+function countBs(s){
+	let count=0;
+	for (let i=0; i<s.length; i++){
+		if(s[i]==='B') count++;
+	}
+	return count;
+}
+console.log(countBs('HelloBB'));
+
+
+function countChar(s,c){
+	let count=0;
+	for (let i=0; i<s.length; i++){
+		if(s[i]===c) count++;
+	}
+	return count;
+}
+console.log(countChar("Hello","l"));
